16711309a513abc354153cb866624269b6b1e96a
cont1nuity <continuity@gmx.net>
2025-02-10 13:21:31 +0100

    Adding generic approach for transmog items.
    Closes #31, thank you for the suggestion Yoshimo!

--- nop-item.lua
+++ nop-item.lua
@@ -123,6 +123,17 @@ function NOP:ItemGetPattern(itemID,bag,slot) -- looking for usable item via patt
     self:Verbose("ItemGetPattern:","itemID",itemID,name,"will be shown as MOUNT")
     return 1, P.PRIO_OPEN --fallback for mounts
   end
+  if classID == Enum.ItemClass.Consumable and subclassID == Enum.ItemConsumableSubclass.Other then
+    local tMogSet = C_Item.GetItemLearnTransmogSet(itemID)
+    if tMogSet then
+      for _, appearance in pairs(C_Transmog.GetAllSetAppearancesByID(tMogSet)) do
+        if appearance and appearance.itemModifiedAppearanceID and not C_TransmogCollection.PlayerHasTransmogItemModifiedAppearance(appearance.itemModifiedAppearanceID) then
+          self:Verbose("ItemGetPattern:","itemID",itemID,name,"will be shown as TRANSMOG")
+          return 1, P.PRI_OPEN --fallback for appearances
+        end
+      end
+    end
+  end
   local n, p = self:ItemGetLockPattern(itemID, lines)
   if n and n > 0 then return n, p end
   for i=1,#lines do -- scan all lines in tooltip
